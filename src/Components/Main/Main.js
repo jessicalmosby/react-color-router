de86c801058ed2307eb992fb6044be431bffcb2c
Jessica Mosby <jessicamosby@mac.com>
2023-01-24 19:17:03 -0500

    added NotFound component and a home route into Main.js, touched up some css

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Main.css';
 import Colors from '../Colors/Colors.js';
 import { Route, Switch } from 'react-router-dom';
+import NotFound from '../NotFound/NotFound.js';
 
 export default function Main() {
   return (
     <main>
       <Switch>
+        <Route exact path="/" />
         <Route exact path="/rgb/:red/:green/:blue" component={Colors} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </main>
   );
